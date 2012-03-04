d7fd5e49c3948769f8794470ad00fbfc07266b2e
Rohen Peterson <rohen@crowdmob.com>
2012-03-04 12:13:48 -0800

    controller and method checking for user

--- lib/devise/strategies/passwordless_authenticatable.rb
+++ lib/devise/strategies/passwordless_authenticatable.rb
@@ -5,13 +5,24 @@ module Devise
     # Default strategy for signing in a user, based on his email and password in the database.
     class PasswordlessAuthenticatable < Authenticatable
       def authenticate!
-        resource = mapping.to.find_for_database_authentication(authentication_hash)
+        if params[:controller] == 'users' && (['create', 'new'].include? params[:action])
+          resource = mapping.to.find_for_database_authentication(authentication_hash)
 
-        if resource
-          resource.after_database_authentication
-          success!(resource)
-        elsif !halted?
-          fail(:invalid)
+          if resource
+            resource.after_database_authentication
+            success!(resource)
+          elsif !halted?
+            fail(:invalid)
+          end
+        else
+          resource = valid_password? && mapping.to.find_for_database_authentication(authentication_hash)
+
+          if validate(resource){ resource.valid_password?(password) }
+            resource.after_database_authentication
+            success!(resource)
+          elsif !halted?
+            fail(:invalid)
+          end
         end
       end
     end
