607235260d1e491f323aa5d7a2e1ba5cbd7c4c8b
Rohen Peterson <rohen@crowdmob.com>
2012-03-04 18:47:45 -0800

    nested check for instance variable

--- lib/devise/strategies/passwordless_authenticatable.rb
+++ lib/devise/strategies/passwordless_authenticatable.rb
@@ -7,7 +7,7 @@ module Devise
       def authenticate!
         resource = mapping.to.find_for_database_authentication(authentication_hash)
 
-        if resource.respond_to?(password_initialized)
+        if resource.respond_to?(:password_initialized)
           if resource.password_initialized
             resource = valid_password? && resource
           
@@ -18,9 +18,12 @@ module Devise
               fail(:invalid)
             end
           end
-        else resource
-          resource.after_database_authentication
-          success!(resource)
+          else resource
+            resource.after_database_authentication
+            success!(resource)
+          end
+        else
+          fail(:invalid)
         end
       end
     end
